7b68d3193f93018e7edba2a5bd6d45b94e51c4a3
AiriNarita <airiswim.kitty@gmail.com>
2023-03-06 11:32:52 +0900

    feat: bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application" 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
