24910c72e0a1570635bc7d08b877937369405a36
AiriNarita <airiswim.kitty@gmail.com>
2023-03-07 12:55:04 +0900

    feat: user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #いいね、投稿、コメント機能のassociation
+  has_many :favorites, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+         
+  #イベント関連のassociation
+  has_many :attendees
+  has_many :events, through: :attendees
+  has_many :created_events, class_name: "Event", foreign_key: "creator_id"
+         
+  #ActiveStrageの設定
+  has_one_attached : image         
 end
