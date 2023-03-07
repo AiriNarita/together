a32e01789f7fb60630b231cf385821e9b71b4125
AiriNarita <airiswim.kitty@gmail.com>
2023-03-07 13:31:29 +0900

    feat: favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_07_042305) do
+ActiveRecord::Schema.define(version: 2023_03_07_042848) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 2023_03_07_042305) do
     t.string "creater", null: false
     t.integer "Sharing_status", default: 0, null: false
     t.text "url", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
