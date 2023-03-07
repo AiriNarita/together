9f1b74b55f328a925d7fd0109302a484d4195c24
AiriNarita <airiswim.kitty@gmail.com>
2023-03-07 23:52:53 +0900

    feat: routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,45 @@
 Rails.application.routes.draw do
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
                        registrations: "public/registrations",
                        sessions: "public/sessions",
                      }
+  scope module: :public do
+    root to: "homes#top"
+    get "/about" => "homes#about"
+    resources :posts do
+      collection do
+        get "confirm"
+      end
+      resources :favorites, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+      resources :hashtags, only: [:create]
+      resources :post_hashtags, only: [:create, :destroy]
+    end
+    resource :users, only: [] do
+      get "my_page" => "users#show"
+      get "information/edit" => "customers#edit"
+      patch "information" => "customers#update"
+    end
+
+    resources :events do
+      resources :attendees, only: [:create, :destroy]
+    end
+  end
 
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
                        sessions: "admin/sessions",
                      }
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :posts do
+      resources :comments, only: [:index, :show, :edit, :update, :destroy]
+    end
+    resources :events, only: [:index, :show]
+  end
 end
