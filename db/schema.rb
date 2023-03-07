f904e82f3c4057cd51e3c7dc58df462b557c0c99
AiriNarita <airiswim.kitty@gmail.com>
2023-03-07 13:42:31 +0900

    feat: Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_07_043418) do
+ActiveRecord::Schema.define(version: 2023_03_07_043556) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -80,8 +80,17 @@ ActiveRecord::Schema.define(version: 2023_03_07_043418) do
   end
 
   create_table "post_comments", force: :cascade do |t|
+    t.text "comment", null: false
     t.integer "post_id", null: false
     t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
