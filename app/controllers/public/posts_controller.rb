a2f0c0937e1a1aa79d05d8b6e788ed40180927dd
AiriNarita <airiswim.kitty@gmail.com>
2023-03-08 19:26:12 +0900

    feat: post new form

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,29 @@
 class Public::PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :post_status)
+  end
 end
