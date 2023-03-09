27ee4fd67ce7c53dfe56b1a95422981af5a10782
AiriNarita <airiswim.kitty@gmail.com>
2023-03-09 13:15:21 +0900

    fix: post_status-drafts/polished

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -4,11 +4,15 @@ class Public::PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.build(post_params)
     if params[:commit] == "下書き保存"
-      @post.save_draft
-      redirect_to posts_path
+      @post = current_user.posts.build(post_params)
+      if @post.save_draft
+        redirect_to drafts_posts_path
+      else
+        render :new
+      end
     else
+      @post = current_user.posts.build(post_params)
       if @post.save
         redirect_to post_path(@post)
       else
@@ -18,7 +22,9 @@ class Public::PostsController < ApplicationController
   end
 
   def drafts
-    @drafts = current_user.posts.draft
+    # 本文に"Ruby"という文字列が含まれ、かつ公開ステータスである投稿を取得
+    @published_posts = Post.where(user_id: current_user.id).where(post_status: :published).order(created_at: :desc)
+    @draft_posts = Post.where(user_id: current_user.id).where(post_status: :draft).order(created_at: :desc)
   end
 
   def show
@@ -29,12 +35,29 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      if params[:commit] == "下書き保存"
+        @post.update(post_status: :draft)
+        redirect_to posts_path, notice: "下書きを保存しました。"
+      else
+        @post.update(post_status: :published)
+        redirect_to @post, notice: "投稿を更新しました。"
+      end
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
