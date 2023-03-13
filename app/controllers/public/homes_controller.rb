16efecc14a4ec66e3f0bfdec151c306de288632c
AiriNarita <airiswim.kitty@gmail.com>
2023-03-13 23:02:08 +0900

    fix: top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,6 @@
 class Public::HomesController < ApplicationController
+  def top
+    @posts = Post.order(likes_count: :desc).limit(4)
+    @events = Event.order(attendees_count: :desc).limit(4)
+  end
 end
