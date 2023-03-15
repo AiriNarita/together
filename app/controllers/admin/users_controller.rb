e1b68c3b5d9e105f6498299ef7455868c3b72282
AiriNarita <airiswim.kitty@gmail.com>
2023-03-15 12:20:47 +0900

    feat: report admin show view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,31 @@ class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  # user_status管理
+
+  def edit
+    @user = User.find(params[:id]) #編集したいuserのIDを取得
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if params[:user] && params[:user][:user_status] == true
+      @user.user_status = true
+    else
+      @user.user_status = false
+    end
+
+    if @user.save
+      redirect_to admin_user_path(@user)
+    else
+      render "show"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_status).transform_values { |value| value == "true" }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -131,7 +131,7 @@ ActiveRecord::Schema.define(version: 2023_03_14_041615) do
     t.string "first_name_kana", default: "", null: false
     t.string "last_name_kana", default: "", null: false
     t.string "introduction"
-    t.integer "user_status", default: 0
+    t.boolean "user_status", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
