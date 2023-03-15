024a3d6e81c25c2d443ac374b7113bd95d9ed36b
AiriNarita <airiswim.kitty@gmail.com>
2023-03-15 10:18:11 +0900

    feat: report　admin index/show

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,2 +1,22 @@
 class Admin::ReportsController < ApplicationController
+  def index
+    @reports = Report.all
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def update
+    @report = Report.find(params[:id])
+    if @report.update(report_params)
+      redirect_to admin_reports_path
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:checked)
+  end
 end
