e489a8a3ced58c81b40da97bf2f36c33c3a22609
AiriNarita <airiswim.kitty@gmail.com>
2023-03-15 23:50:07 +0900

    feat: add japan time_zone

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Together
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
 
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
