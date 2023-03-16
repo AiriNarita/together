f71210e2924ea6ff611022bf7bea9d513c69a800
AiriNarita <airiswim.kitty@gmail.com>
2023-03-16 23:14:37 +0900

    fix: events controller

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -6,11 +6,5 @@ class Admin::EventsController < ApplicationController
 
   def index
     @events = Event.visible.page(params[:page]).per(8)
-    case params[:sort]
-    when "attendee"
-      @events = @events.order(attendee_count: :desc)
-    when "latest"
-      @events = @events.order(created_at: :desc)
-    end
   end
 end
