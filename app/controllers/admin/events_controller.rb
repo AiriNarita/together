a1953b52f2256ebec98aa62d827fff04fe3fa753
AiriNarita <airiswim.kitty@gmail.com>
2023-03-16 17:11:10 +0900

    feat: admin/events show

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,6 @@
 class Admin::EventsController < ApplicationController
+  def show
+    @event = Event.find(params[:id])
+    @attendee = Attendee.find_by(user_id: current_user.id, event_id: @event.id)
+  end
 end
