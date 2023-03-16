59d5c5711eca60a242ee1fca7c14638a1395a132
AiriNarita <airiswim.kitty@gmail.com>
2023-03-16 14:46:33 +0900

    feat: post index

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,15 @@
 class Admin::PostsController < ApplicationController
+  def index
+    @posts = Post.visible
+
+    case params[:sort]
+    when "favorites"
+      @posts = @posts.order(favorites_count: :desc)
+    when "latest"
+      @posts = @posts.order(created_at: :desc)
+    end
+  end
+
+  def show
+  end
 end
