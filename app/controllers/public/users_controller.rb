78a0b2015b4c889433c40cf097b5b8d94ff5f8a1
AiriNarita <airiswim.kitty@gmail.com>
2023-03-19 15:15:53 +0900

    feat: follow/follower index view

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -41,11 +41,16 @@ class Public::UsersController < ApplicationController
   def follows
     @user = User.find(params[:user_id])
     @users = @user.following_user.page(params[:page]).per(3).reverse_order
+
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def followers
     @user = User.find(params[:user_id])
     @users = @user.follower_user.page(params[:page]).per(3).reverse_order
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   private
