b28ecad1ce1c0dfa02ac23779024ce8b96d6ac0f
AiriNarita <airiswim.kitty@gmail.com>
2023-03-20 09:51:05 +0900

    fix: events controller

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -57,8 +57,11 @@ class Public::EventsController < ApplicationController
   def myevent
     #countをviewではなくここで書いたのは、pagenationなどに影響されないため
     @createdCount = Event.joins(:creator).where(creator: { id: current_user.id }).count
-    @attendedCount = Event.joins(:creator).where(creator: { id: current_user.id }).where.not(creator_id: current_user.id).count
-    @pastAttendedCount = Event.joins(:creator).where(creator: { id: current_user.id }).where("date < ?", Time.now).count
+    @upcoming = Event.joins(:creator).where(creator: { id: current_user.id }).where("date >= ?", Time.now).count
+    @past_events = Event.joins(:creator).where(creator: { id: current_user.id }).where("date < ?", Time.now).count
+    @attendedCount = Event.joins(:attendees).where(attendees: { user_id: current_user.id }).where.not(creator_id: current_user.id).count
+    @pastAttendedCount = Event.joins(:attendees).where(attendees: { user_id: current_user.id }).where("date < ?", Time.now).where.not(creator_id: current_user.id).count
+
     if params[:event_type] == "created_events"
       @myevents = Event.joins(:creator).where(creator: { id: current_user.id }).page(params[:page]).per(8)
     elsif params[:event_type] == "upcoming_events"
@@ -66,11 +69,11 @@ class Public::EventsController < ApplicationController
     elsif params[:event_type] == "past_events"
       @myevents = Event.joins(:creator).where(creator: { id: current_user.id }).where("date < ?", Time.now).page(params[:page]).per(8)
     elsif params[:event_type] == "attended_events"
-      @myevents = Event.joins(:creator).where(creator: { id: current_user.id }).where.not(creator_id: current_user.id).page(params[:page]).per(8)
+      @myevents = Event.joins(:attendees).where(attendees: { user_id: current_user.id }).where.not(creator_id: current_user.id).page(params[:page]).per(8)
     elsif params[:event_type] == "past_attended_events"
-      @myevents = Event.joins(:creator).where(creator: { id: current_user.id }).where("date < ?", Time.now).where.not(creator_id: current_user.id).page(params[:page]).per(8)
+      @myevents = Event.joins(:attendees).where(attendees: { user_id: current_user.id }).where("date < ?", Time.now).where.not(creator_id: current_user.id).page(params[:page]).per(8)
     else
-      @myevents = Event.joins(:creator).where(creator: { id: current_user.id }).page(params[:page]).per(8)
+      @myevents = Event.joins(:attendees).where(attendees: { user_id: current_user.id }).page(params[:page]).per(8)
     end
   end
 
