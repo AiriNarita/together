39bb17d6523a4007bbcacb78e76ffefa25b7ed18
AiriNarita <airiswim.kitty@gmail.com>
2023-03-23 10:16:40 +0900

    fix: hashtag link , css

--- app/controllers/public/hashtags_controller.rb
+++ app/controllers/public/hashtags_controller.rb
@@ -2,9 +2,9 @@ class Public::HashtagsController < ApplicationController
   def show
     #@hashtag = Hashtag.find_by(hashtag_name: params[:hashtag_name])
     #@posts = Post.joins(:hashtags).where(hashtags: { hashtag_name: params[:hashtag_name] }).order(created_at: :desc)
-
+    @hashtags = Post.order(created_at: :desc).page(params[:page]).per(8)
     @hashtag = Hashtag.find_by(id: params[:id])
-
+    @post = Post.find(params[:id])
     if @hashtag.present?
       @posts = Post.joins(:hashtags).where(hashtags: { id: params[:id] }).order(created_at: :desc)
     else
