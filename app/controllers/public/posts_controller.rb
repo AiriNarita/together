9dc418060dad1d6dc24e6c46828cc5de7833440f
AiriNarita <airiswim.kitty@gmail.com>
2023-03-25 19:54:24 +0900

    fix: event post/draft button

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,6 +6,7 @@ class Public::PostsController < ApplicationController
   end
 
   def create
+
     # 下書き保存ボタンを押されたら
     if params[:commit] == "下書き保存"
       @post = Post.create(post_params.merge(user_id: current_user.id))
