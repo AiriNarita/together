5d6babba57faa796397a0567863d1bcce25c1c97
AiriNarita <airiswim.kitty@gmail.com>
2023-03-28 01:24:47 +0900

    fix: ui bugs
    - event card
    - hashtag

--- app/controllers/public/hashtags_controller.rb
+++ app/controllers/public/hashtags_controller.rb
@@ -1,14 +1,6 @@
 class Public::HashtagsController < ApplicationController
   def show
-    #@hashtag = Hashtag.find_by(hashtag_name: params[:hashtag_name])
-    #@posts = Post.joins(:hashtags).where(hashtags: { hashtag_name: params[:hashtag_name] }).order(created_at: :desc)
-    @hashtags = Post.order(created_at: :desc).page(params[:page]).per(8)
     @hashtag = Hashtag.find_by(id: params[:id])
-    @post = Post.find(params[:id])
-    if @hashtag.present?
-      @posts = Post.joins(:hashtags).where(hashtags: { id: params[:id] }).order(created_at: :desc)
-    else
-      @posts = []
-    end
+    @posts = Post.joins(:hashtags).where(hashtags: { id: params[:id] }).order(created_at: :desc).page(params[:page]).per(8)
   end
 end
