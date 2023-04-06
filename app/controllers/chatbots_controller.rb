7eaebab1c99034b8ffa4c22edd1bc82409f73132
AiriNarita <airiswim.kitty@gmail.com>
2023-04-06 23:00:54 +0900

    fix: chatbot

--- app/controllers/chatbots_controller.rb
+++ app/controllers/chatbots_controller.rb
@@ -3,7 +3,7 @@ class ChatbotsController < ApplicationController
 
   def create
     input = params[:input]
-    client = OpenAI::Client.new(access_token: ENV['OPENAI_ACCESS_TOKEN'])
+    client = OpenAI::Client.new(access_token: ENV["OPENAI_ACCESS_TOKEN"])
     # Define logic to generate response based on user input
     # https://platform.openai.com/docs/api-reference/chat/create
     response = client.chat(
@@ -11,8 +11,9 @@ class ChatbotsController < ApplicationController
         model: "gpt-3.5-turbo", # Required.
         messages: [{ role: "system", content: "You are a helpful assistant. response english to japanese" }, { role: "user", content: input }], # Required.
         temperature: 0.7,
-        max_tokens: 200
-      })
+        max_tokens: 200,
+      },
+    )
 
     respond_to do |format|
       format.json { render json: { response: response.dig("choices", 0, "message", "content") } }
